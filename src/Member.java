00b5dbf4122b60d18a203a972634fe4fc04bea1c
DNY <dannylu1999@gmail.com>
2021-11-16 14:44:22 +0100

    Added SubscriptionType + AgeGroup enum. Added misc

--- src/Member.java
+++ src/Member.java
@@ -1,68 +1,104 @@
+// Lu og Dannie
 public class Member {
 
-  private String memberName;
-    private int memberYearOfBirth;
-    //Personal info
+    enum SubscriptionType {
+        ACTIVE("Aktiv"),
+        PASSIVE("Passiv");
 
+        SubscriptionType(String type) {
+        }
+    }
+
+    enum AgeGroup {
+        JUNIOR("Junior"),
+        SENIOR("Senior");
+
+        AgeGroup(String ageGroup) {
+        }
+    }
+
+    //Pris variabler
+    private int seniorPrice = 1600;
+    private int juniorPrice = 1000;
+    private int passivePrice = 500;
+    private double seniorDiscountPrice = seniorPrice * 0.25;
+
+    //Personal info
+    private int memberYearOfBirth;
+    private String memberName;
 
     // Jeg har lige lavet et par ændringer, så det måske passer bedre til det jeg skal lave -Lu
-    // Swimming info
-    private String activityForm;
-    private String swimmerTitle; // Ved ikke hvad det skal hedde (Junior eller senior)
-    private String swimmerType;
+
+    // Info om svømmeren
+    private SubscriptionType subscriptionType; // Aktiv eller passiv
+    private AgeGroup ageGroup; // Junior eller Senior
+    private String swimmerType; // Motionist eller konkurrencesvømmer
 
     // Payment info
     private double paymentAmount;
-    private boolean paidSubscription;
+    private boolean paidSubscription; // Ændres senere
 
     private String discipline;
     private double bestTime; // Find et bedre ord for det
 
-  //2021 - Fjernes senere når vi bliver klogere
-  private int currentYear = 2021;
+    //2021 - Fjernes senere når vi bliver klogere
+    private int currentYear = 2021;
 
-  public Member(String memberName, int memberYearOfBirth, String activityForm,
-                String swimmerTitle, String swimmerType, String discipline, double bestTime) {
-    this.memberName = memberName;
-    this.memberYearOfBirth = memberYearOfBirth;
-    this.activityForm = activityForm;
-    this.swimmerTitle = swimmerTitle;
+    public Member(String memberName, int memberYearOfBirth, SubscriptionType subscriptionType, String swimmerType, String discipline, double bestTime) {
+        this.memberName = memberName;
+        this.memberYearOfBirth = memberYearOfBirth;
+        this.subscriptionType = subscriptionType;
+        this.swimmerType = swimmerType;
+        this.discipline = discipline;
+        this.bestTime = bestTime;
 
-    // Swimmer i forhold til alderen
-    if (currentYear - this.memberYearOfBirth >= 18) {
-      this.swimmerTitle = "senior";
-    } else {
-      this.swimmerTitle = "junior";
+
+        // Swimmer i forhold til alderen
+        if (currentYear - this.memberYearOfBirth >= 18) {
+            this.ageGroup = AgeGroup.SENIOR;
+        } else {
+            this.ageGroup = AgeGroup.JUNIOR;
+        }
+
+        // Betaling i forhold til "Title" og alder
+        if (this.subscriptionType == SubscriptionType.ACTIVE) {
+            if (currentYear - this.memberYearOfBirth < 18) { // Hvis man er under 18 år, koster det 1000 kr.
+                this.paymentAmount = juniorPrice; // 1000 kr.
+            } else if (currentYear - this.memberYearOfBirth >= 60) { // Hvis man er over 60 år, koster det 1200 kr. da der er 25% rabat på seniorprisen (1600 kr.)
+                this.paymentAmount = seniorDiscountPrice; // 25% rabat = 400
+            } else {
+                this.paymentAmount = seniorPrice; // Hvis man er mellem 18 og 60 år gammel, koster det 1600 kr.
+            }
+        } else { // Hvis man har et passivt medlemskab
+            this.paymentAmount = passivePrice; // 500 kr.
+        }
+        this.paidSubscription = false; // Hvis subscription ikke er aktivt
+        this.discipline = discipline;
+        this.bestTime = bestTime;
+
     }
 
-    // Betaling i forhold til "Title" og alder
-    if (this.activityForm.equals("active")) {
-      if (currentYear - this.memberYearOfBirth < 18) { // Hvis man er under 18 år, koster det 1000 kr.
-        this.paymentAmount = 1000;
-      } else if (currentYear - this.memberYearOfBirth >= 60) { // Hvis man er over 60 år, koster det 500 kr.
-        this.paymentAmount = 500;
-      } else {
-        this.paymentAmount = 1600; // Hvis man er mellem 18 og 60 år gammel, koster det 1600 kr.
-      }
+    public void paidSubscription() {
+        this.paidSubscription = true;
     }
 
-  }
-
-
-
-  public void setMemberName() {
-    this.memberName = memberName;
-  }
-  public String getMemberName() {
-    return memberName;
-  }
-
-  public void setMemberYearOfBirth() {
-    this.memberYearOfBirth = memberYearOfBirth;
-  }
-  public int getMemberYearOfBirth() {
-    return memberYearOfBirth;
-  }
-
-
+    // Getters
+    public String getMemberName() {
+        return memberName;
+    }
+    public int getMemberYearOfBirth() {
+        return memberYearOfBirth;
+    }
+    public SubscriptionType getSubscriptionType() {
+        return subscriptionType;
+    }
+    public AgeGroup getAgeGroup() {
+        return ageGroup;
+    }
+    public String getSwimmerType() {
+        return swimmerType;
+    }
+    public double getPaymentAmount() {
+        return paymentAmount;
+    }
 }
