d815c2655523feb38c3d6f05ca77cb19c0f178e2
david <david.bjakobsen@gmail.com>
2021-11-16 11:11:56 +0100

    Renamed Main.java to Dolphin.java Created an instance of Dolphin

---
+++ src/Dolphin.java
@@ -0,0 +1,10 @@
+public class Dolphin {
+    public static void main(String[] args) {
+        Dolphin dolphin = new Dolphin();
+        dolphin.run();
+    }
+
+    private void run() {
+
+    }
+}
