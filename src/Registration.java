f8daa8c6f70b1a598456dd596ccf41807347d3db
UmaidJS <91475875+UmaidJS@users.noreply.github.com>
2021-11-18 15:04:53 +0100

    registra

--- src/Registration.java
+++ src/Registration.java
@@ -1,10 +1,18 @@
 public class Registration {
-
+  UserInput userInput = new UserInput();
   /*
     Denne klasse skal kunne registrerer svømmerne til stævner og angive placering og tid.
     Derudover skal der vises top 5 svømmer for hver disciplin.
    */
 
 
+  private void timeRegistration() {
+    double lapTime;
+    System.out.println("Indtast svømmerens tid: ");
+    lapTime = userInput.getDoubleInput();
+    System.out.println(lapTime);
 
+
+
+  }
 }
