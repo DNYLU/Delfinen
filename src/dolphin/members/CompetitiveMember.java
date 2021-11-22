8ddabf383fa1f3ec448c86e3cc3e422f9d24d3ea
david <david.bjakobsen@gmail.com>
2021-11-22 14:31:17 +0100

    refactored constructor

--- src/dolphin/members/CompetitiveMember.java
+++ src/dolphin/members/CompetitiveMember.java
@@ -7,8 +7,9 @@ public class CompetitiveMember extends Member {
 
     private ArrayList<Discipline> disciplines;
 
-    public CompetitiveMember(String memberName, int memberYearOfBirth, SubscriptionType subscriptionType, String swimmerType) {
+    public CompetitiveMember(String memberName, int memberYearOfBirth, SubscriptionType subscriptionType, String swimmerType, ArrayList<Discipline> discipline) {
         super(memberName, memberYearOfBirth, subscriptionType, swimmerType);
+        this.disciplines = discipline;
 
     }
 
