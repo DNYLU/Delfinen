5f0c73f4be06f53e868ccdb116809f1972bf118f
david <david.bjakobsen@gmail.com>
2021-11-24 13:24:52 +0100

    Changed the discount age to the correct value

--- src/dolphin/members/Subscription.java
+++ src/dolphin/members/Subscription.java
@@ -21,7 +21,7 @@ public class Subscription {
             if (ageGroup == AgeGroup.JUNIOR) { // Hvis man er under 18 år, koster det 1000 kr.
                 this.paymentAmount = juniorPrice; // 1000 kr.
             } else { // Hvis man er over 60 år, koster det 1200 kr. da der er 25% rabat på seniorprisen (1600 kr.)
-                if (age >= 65) {
+                if (age >= 60) {
                     this.paymentAmount = this.seniorDiscountPrice;
                 } else {
                     this.paymentAmount = this.seniorPrice; // 25% rabat = 400
