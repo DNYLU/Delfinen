069e4f81cf66714e6b1541260dd1e1bc2958635d
david <david.bjakobsen@gmail.com>
2021-11-30 11:33:05 +0100

    Created a new constructor for SwimmingTime.java and gave it setters for it's attributes.

--- src/dolphin/util/SwimmingTime.java
+++ src/dolphin/util/SwimmingTime.java
@@ -5,6 +5,16 @@ public class SwimmingTime {
     int seconds;
     int milliSeconds;
 
+    public SwimmingTime() {
+
+    }
+
+    public SwimmingTime(int minutes, int seconds, int milliSeconds) {
+        this.setMinutes(minutes);
+        this.setSeconds(seconds);
+        this.setMilliSeconds(milliSeconds);
+    }
+
     public String toString() {
         return this.minutes + ":" + this.seconds + ":" + this.milliSeconds;
     }
@@ -16,5 +26,23 @@ public class SwimmingTime {
         totalTime += milliSeconds*0.001;
 
         return totalTime;
+    }
+
+    public void setMinutes(int minutes) {
+        if (minutes > 0) {
+            this.minutes = minutes;
+        }
+    }
+
+    public void setSeconds(int seconds) {
+        if (seconds > 0) {
+            this.seconds = seconds;
+        }
+    }
+
+    public void setMilliSeconds(int milliSeconds) {
+        if (milliSeconds > 0) {
+            this.milliSeconds = milliSeconds;
+        }
     }
 }
